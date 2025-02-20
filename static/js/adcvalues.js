1f0bab325a5da404a15ff987bc7d98b7c0d4e507
laurentbrune118 <laurentherwartbrune@gmail.com>
2025-02-20 15:11:50 +0100

    Update adcvalues.js

--- static/js/adcvalues.js
+++ static/js/adcvalues.js
@@ -1,13 +1,15 @@
 // Define global variables for adc0 through adc7
 let adc0, adc1, adc2, adc3, adc4, adc5, adc6, adc7;
+const databaseUrl = "https://flow-field-demonstrator-default-rtdb.europe-west1.firebasedatabase.app/";  // Firebase URL
 
 // Fetch sensor data from the Python server every second
 setInterval(async () => {
   try {
-    const response = await fetch('http://127.0.0.1:5000/dataavg');
-    const data = await response.json();
+    const response = await fetch(databaseUrl); // Fetching Data
+    const data = await response.json(); // Converting response to JSON
 
-    // Update each ADC value in the corresponding HTML element and global variables
+
+    // Loop through ADC values and update the webpage
     for (let i = 0; i < 8; i++) {
       const adcElement = document.getElementById(`adc${i}`);
       if (adcElement) {
@@ -17,6 +19,6 @@ setInterval(async () => {
       window[`adc${i}`] = data[`adc${i}`] || null;
     }
   } catch (e) {
-    console.error("Error fetching or parsing data:", e);
+    console.error("Error fetching RTDB data:", e);
   }
 }, 1000);  // Fetch new data every second
